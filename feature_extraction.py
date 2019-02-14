40c2ec25a8e05bcc38268de3eee6336c7c68d74b
anujanegi <anujanegi1@gmail.com>
2019-02-14 16:44:23 +0530

    Feature extraction for single image

--- feature_extraction.py
+++ feature_extraction.py
@@ -25,3 +25,20 @@ def extractFeatures(directory):
         features[file.split('.')[0]] = model.predict(image, verbose = 0)
 
     return features
+
+
+def extractFeature(filename):
+
+    # defining model
+    model = VGG16()
+    # removing the last layer
+    model.layers.pop()
+    model = Model(inputs=model.inputs, outputs=model.layers[-1].output)
+
+    image = load_img(filename, target_size=(224, 224))
+    image = img_to_array(image)
+    image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+    image = preprocess_input(image)
+    feature = model.predict(image, verbose = 0)
+
+    return feature
